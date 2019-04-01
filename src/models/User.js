ac18133e24e3d5627c27ab99f871930ec2c0a442
Sam Martorana <sam.martorana@brightpeakfinancial.com>
2019-04-01 15:27:33 -0500

    Basic rendition of mithril tutorial with simple html

---
+++ src/models/User.js
@@ -0,0 +1,10 @@
+let m = require("mithril")
+
+let User = {
+    list: [],
+    loadList: function () {
+        // TODO: make XHR call
+    }
+}
+
+module.exports = User
---
+++ src/views/UserList.js
@@ -0,0 +1,11 @@
+let m = require("mithril")
+let User = require("../models/User")
+
+module.exports = {
+    oninit: User.loadList,
+    view: function () {
+        return m(".user-list", User.list.map(function (user) {
+            return m(".user-list-item", user.firstName + " " + user.lastName)
+        }))
+    }
+}
