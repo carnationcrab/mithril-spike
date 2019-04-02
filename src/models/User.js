31f6dffad1e4fb4ccabadb748476448ee1bc2dcf
Sam Martorana <sam.martorana@brightpeakfinancial.com>
2019-04-02 14:56:50 -0500

    not yet functional

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,37 @@
-let m = require("mithril")
+var m = require("mithril")
 
 let User = {
     list: [],
     loadList: function () {
-        // TODO: make XHR call
+        return m.request({
+                method: "GET",
+                url: "https://rem-rest-api.herokuapp.com/api/users",
+                withCredentials: true,
+            })
+            .then(function (result) {
+                User.list = result.data
+            })
+    },
+
+    current: {},
+    load: function (id) {
+        return m.request({
+                method: "GET",
+                url: "https://rem-rest-api.herokuapp.com/api/users/" + id,
+                withCredentials: true,
+            })
+            .then(function (result) {
+                User.current = result
+            })
+    },
+
+    save: function () {
+        return m.request({
+            method: "PUT",
+            url: "https://rem-rest-api.herokuapp.com/api/users/" + User.current.id,
+            data: User.current,
+            withCredentials: true,
+        })
     }
 }
 
--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,5 +1,32 @@
+var m = require("mithril")
+let User = require("../models/User")
+
 module.exports = {
+    oninit: function (vnode) {
+        User.load(vnode.attrs.id)
+    },
     view: function () {
-        // TODO implement view
+        return m("form", {
+            onsubmit: function (e) {
+                e.preventDefault()
+                User.save()
+            }
+        }, [
+            m("label.label", "First name"),
+            m("input.input[type=text][placeholder=First name]", {
+                oninput: function (e) {
+                    User.current.firstName = e.target.value
+                },
+                value: User.current.firstName
+            }),
+            m("label.label", "Last name"),
+            m("input.input[placeholder=Last name]", {
+                oninput: function (e) {
+                    User.current.lastName = e.target.value
+                },
+                value: User.current.lastName
+            }),
+            m("button.button[type=submit]", "Save"),
+        ])
     }
 }
